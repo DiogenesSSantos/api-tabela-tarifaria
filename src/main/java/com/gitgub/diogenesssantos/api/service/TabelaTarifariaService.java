d780f87a0c902c4d97ee27462a3b895295a1e84f
Diogenes santos <diogenescontatoofficial@hotmail.com>
2026-06-03 19:15:42 -0300

    Melhorando a performace do código ao salvar faixasTarifarias, tanto para código individual ou salvamento em

--- src/main/java/com/gitgub/diogenesssantos/api/service/TabelaTarifariaService.java
+++ src/main/java/com/gitgub/diogenesssantos/api/service/TabelaTarifariaService.java
@@ -73,14 +73,45 @@ public class TabelaTarifariaService {
 
     @Transactional
     public List<TabelaTarifaria> salvarTabelaEmLote(List<TabelaTarifariaRequestDTO> tabelaTarifariaRequests) {
-        List<TabelaTarifaria> tabelaTarifariaList = new ArrayList<>();
+        tabelaTarifariaRequests.stream().forEach(this::validaFaixasTabelaTarifaria);
 
-        for (var tabelaTarifaria : tabelaTarifariaRequests) {
-            var tabelaTarSalvaBD = salvarTabela(tabelaTarifaria);
-            tabelaTarifariaList.add(tabelaTarSalvaBD);
+        List<TabelaTarifaria> tabelas = tabelaTarifariaRequests.stream()
+                .map(req -> {
+                    TabelaTarifaria tabela = new TabelaTarifaria();
+                    tabela.setNome(req.nome());
+                    tabela.setDataVigencia(req.dataVigencia());
+                    tabela.setAtivo(true);
+                    return tabela;
+                })
+                .toList();
+
+        List<TabelaTarifaria> tabelasSalvas = tabelaRepo.saveAllAndFlush(tabelas);
+        List<FaixaTarifaria> faixaTarifariaList = new ArrayList<>();
+
+        for (int i = 0; i < tabelasSalvas.size(); i++) {
+            TabelaTarifariaRequestDTO tabelaTarifariaRequestDTO = tabelaTarifariaRequests.get(i);
+            TabelaTarifaria tabelaTarifaria = tabelasSalvas.get(i);
+
+
+            for (CategoriaRequestDTO catReq : tabelaTarifariaRequestDTO.categorias()) {
+                Categoria categoria = Categoria.valueOf(catReq.nome().toUpperCase());
+
+                for (FaixaRequestDTO faixaReq : catReq.faixas()) {
+                    FaixaTarifaria faixa = new FaixaTarifaria();
+                    faixa.setTabela(tabelaTarifaria);
+                    faixa.setCategoria(categoria);
+                    faixa.setInicio(faixaReq.inicio());
+                    faixa.setFim(faixaReq.fim());
+                    faixa.setValorUnitario(BigDecimal.valueOf(faixaReq.valorUnitario()));
+                    faixa.setOrdem(faixaReq.ordem());
+                    faixaTarifariaList.add(faixa);
+                }
+            }
         }
 
-        return tabelaTarifariaList;
+        faixaRepo.saveAll(faixaTarifariaList);
+
+        return tabelasSalvas;
 
     }
 
